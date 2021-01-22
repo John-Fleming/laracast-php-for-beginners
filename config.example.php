d781497038e01021dee72db35ea1609590b8296d
John Fleming <johndeclanfleming@gmail.com>
2021-01-22 15:10:42 -0600

    config example

---
+++ config.example.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'database' => [
+    'name' => 'dbName',
+    'username' => 'root',
+    'password' => '',
+    'connection' => '',
+    'options' => [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+    ]
+  ]
+];
