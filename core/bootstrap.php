332ce6d975c10cba0489d01aef4d4fbddc54108d
John Fleming <johndeclanfleming@gmail.com>
2021-01-25 23:53:44 -0600

    composer and autoloader

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,9 +2,4 @@
 
 $config = require 'config.php';
 
-require 'core/router.php';
-require 'core/request.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
-
 return new QueryBuilder(Connection::make($config['database']));
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'aae360440117b5dc058f515a02d309479435f7af',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aae360440117b5dc058f515a02d309479435f7af',
+    ),
+  ),
+);
